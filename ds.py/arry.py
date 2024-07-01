6914e3c2ba86f0f46f32dc7456cd77eab7f5036a
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-30 23:00:07 -0700

    arry.py

--- ds.py/arry.py
+++ ds.py/arry.py
@@ -10,3 +10,14 @@ print(arr2)
 print(arr3)
 print(arr3.ndim)
 print(arr2.ndim)
+
+import numpy as np
+arr=np.array([[1,2],[3,4]])
+arr
+arr[0,1]
+
+import numpy as np 
+arr=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+arr
+arr[1,0,0]
+
